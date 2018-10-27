f7c80279ec80088d49446b18feb08c501f61b8c3
adekunle <ibbkunle@yahoo.com>
2018-10-28 00:07:17 +0100

    new

---
+++ GitProject/src/test/java/qa/ebay/test/LoginTest.java
@@ -0,0 +1,13 @@
+package qa.ebay.test;
+
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	
+	@Test
+	public void login(){
+		
+		System.out.println("usrename and password");
+	}
+
+}
